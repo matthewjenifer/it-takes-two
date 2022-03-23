cdd379cb9fe32b791a999a52ceed0993980a181e
MATTHEW JENIFER <matthew36.j@gmail.com>
2022-03-23 01:00:59 -0400

    for funzies

---
+++ ittakes2.js
@@ -0,0 +1,12 @@
+var WannaRockRightNow = true;
+var CameToGetDown = false;
+var NotInternationallyKnown = true;
+var knownToRockTheMic = true;
+
+if(WannaRockRightNow && CameToGetDown && NotInternationallyKnown && knownToRockTheMic){
+    console.log("You are Rob Base!");
+} else {
+    console.log("You are not Rob Base");
+}
+
+//inspo: https://www.reddit.com/r/ProgrammerHumor/comments/tkhezt/turns_out_im_rob_base/
